956535f046fb4c6a407d60f58faed8bae300d839
Mojtaba <mojtaba.business.ceo@gmail.com>
2023-05-24 17:00:05 +0430

    Update XLog.php

--- XLog.php
+++ XLog.php
@@ -7,25 +7,31 @@
 			return get_class();
 		}
 
-		public static function log($array, $json = true) {
+		public static function log($array, $json = true, $m_die = true) {
 			if($json)
 				header("Content-Type: application/json");
 
 			echo json_encode($array, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-			die();
+
+			if($m_die)
+				die();
 		}
 
-		public static function p($array, $json = true) {
+		public static function p($array, $json = true, $m_die = true) {
 			if($json)
 				header("Content-Type: application/json");
 
 			print("<br>".print_r($array)."</br>");
-			die();
+
+			if($m_die)
+				die();
 		}
 
-		public static function dump($array) {
+		public static function dump($array, $m_die = true) {
 			var_dump($array);
-			die();
+
+			if($m_die)
+				die();
 		}
 	}
 
