96801043ac03b3ddd6e8b0ffa396a5a51f459ab1
Mojtaba <mojtaba.business.ceo@gmail.com>
2023-05-25 16:07:33 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <?php
 	include_once 'HeaderManagement.php';
 
-
-
 	if(!isset($_SERVER['PATH_INFO']))
 		$m_function->setToken('a9a0849fbc3eb1f0ca2bde125debd4a7a58c80683d9023e621b7c18cb9c7084d');
 	else
 		$m_function->setToken(ltrim($_SERVER['PATH_INFO'], '/'));
 
 	$m_function->__initialization(
-		function() use( $m_function, $m_platform, $m_runtime )
+		function() use( $m_function, $m_platform, $m_runtime, $m_database )
 		{
 			$m_function->_functions['a9a0849fbc3eb1f0ca2bde125debd4a7a58c80683d9023e621b7c18cb9c7084d'] = 'get_about';
 			$m_function->_functions['04089a41989c1b760dffaed2674eb4205997057bc387c1e38977e00a532c2243'] = 'my_example_function';
