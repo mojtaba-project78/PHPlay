38a94cf232c835cf98d0f92783ece8a97adf2527
Mojtaba <mojtaba.business.ceo@gmail.com>
2023-05-26 14:15:46 +0430

    Update XLog.php

--- Core/XLog.php
+++ Core/XLog.php
@@ -1,15 +1,34 @@
 <?php
 
+	include_once 'XPlatform.php';
+
 	class XLog
 	{
+		protected $m_platform = null;
+		public function __construct(XPlatform &$m_platform, XAppRuntime &$m_runtime)
+		{
+			$this->m_platform = $m_platform;
+			$this->m_runtime = $m_runtime;
+		}
+
 		// TODO: get class name
 		public static function className() {
 			return get_class();
 		}
 
-		public static function log($array, $json = true, $m_die = true) {
+		public function __invoke($array, $json = true, $m_die = true)
+		{
+			$this->log($array, $json, $m_die);
+		}
+
+		public function log($array, $json = true, $m_die = true) {
 			if($json)
 				header("Content-Type: application/json");
+
+			$array['m_platform'] = PLATFORM_NAME;
+			$array['m_platform_uri'] = sprintf("%s v%s", PLATFORM_NAME, $this->m_platform->get_version());
+			$array['m_version'] = $this->m_platform->get_version();
+			$array['m_runtime'] = $this->m_runtime->get();
 
 			echo json_encode($array, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
 
@@ -17,7 +36,7 @@
 				die();
 		}
 
-		public static function p($array, $json = true, $m_die = true) {
+		public function p($array, $json = true, $m_die = true) {
 			if($json)
 				header("Content-Type: application/json");
 
@@ -27,7 +46,7 @@
 				die();
 		}
 
-		public static function dump($array, $m_die = true) {
+		public function dump($array, $m_die = true) {
 			var_dump($array);
 
 			if($m_die)
