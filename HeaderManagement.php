95e04d5de17f421631bf0c258aae993765e55592
Mojtaba <mojtaba.business.ceo@gmail.com>
2023-05-26 13:11:03 +0430

    moving to Core folder

--- HeaderManagement.php
+++ HeaderManagement.php
@@ -3,17 +3,16 @@
 
 	//===============================================================================
 	// ### include 'PHPlay' headers here...
-	include_once 'XLog.php';
-	include_once 'XVersionManagement.php';
-	include_once 'XPlatform.php';
-	include_once 'XAppRuntime.php';
-	include_once 'XFunctionManagement.php';
-	include_once 'XErrorManagement.php';
-	include_once 'XDatabaseManagement.php';
+	include_once 'Core/XLog.php';
+	include_once 'Core/XVersionManagement.php';
+	include_once 'Core/XPlatform.php';
+	include_once 'Core/XAppRuntime.php';
+	include_once 'Core/XFunctionManagement.php';
+	include_once 'Core/XErrorManagement.php';
+	include_once 'Core/XDatabaseManagement.php';
 
 	//===============================================================================
 	// ### defined global variables here...
-
 	// ### XPlatform
 	$m_platform = new XPlatform();
 	$m_platform->init_version();
